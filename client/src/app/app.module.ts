1f880525662596dec3acbb51bf311b5600e7db61
Charles <charles.delafontaine@gmail.com>
2022-03-26 00:58:22 -0400

    Fixed imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./modules/app-routing.module";
 import { AppComponent } from "./app.component";
 import { CommunicationService } from "./communication.service";
-import { AppMaterialModule } from './modules/material.module';
+// import { AppMaterialModule } from './modules/material.module';
 import { JardinsComponent } from './jardins/jardins.component';
 import { VarietesComponent } from './varietes/varietes.component';
 import { PlantesComponent } from './plantes/plantes.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { PlantesComponent } from './plantes/plantes.component';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    AppMaterialModule
+    MatSliderModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [CommunicationService],
   bootstrap: [AppComponent],
