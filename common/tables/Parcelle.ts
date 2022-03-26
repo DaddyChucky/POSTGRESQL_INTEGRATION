1ac79446996abaa30f41d4602db09ae781afb8a4
Charles <charles.delafontaine@gmail.com>
2022-03-25 22:09:59 -0400

    Parcelle int

---
+++ common/tables/Parcelle.ts
@@ -0,0 +1,7 @@
+import { Coordonnnes_t } from '../c_types/Coordonnees_t';
+import { Dimensions_t } from '../c_types/Dimensions_t';
+export interface Parcelle {
+    IDJardin: number;
+    coordonnees: Coordonnnes_t;
+    dimensions: Dimensions_t;
+}
