25db0480035acbc76f0a4d9bf3ce800263075771
Charles <charles.delafontaine@gmail.com>
2022-03-25 22:10:03 -0400

    Rang int

---
+++ common/tables/Rang.ts
@@ -0,0 +1,10 @@
+import { Coordonnnes_t } from "../c_types/Coordonnees_t";
+
+export interface Rang {
+    IDJardin: number;
+    coordonneesParcelle: Coordonnnes_t;
+    numero: number;
+    coordonneesRang: Coordonnnes_t;
+    dateDebutJachere: Date;
+    dateFinJachere: Date;
+}
