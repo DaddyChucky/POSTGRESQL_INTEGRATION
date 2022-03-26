0a53fc94efda162ff375e8f729d43d47bcbcca03
Charles <charles.delafontaine@gmail.com>
2022-03-25 22:09:38 -0400

    Jardin int

--- common/tables/Jardin.ts
+++ common/tables/Jardin.ts
@@ -2,5 +2,10 @@ export interface Jardin {
     ID: number;
     nom: string;
     surface: number;
+    bPotager: boolean;
+    bOrnement: boolean;
+    bVerger: boolean;
+    typeSol: string;
+    hauteurMaximale: number;
   }
   
