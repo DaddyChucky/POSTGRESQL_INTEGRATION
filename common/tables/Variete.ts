b7bca52a01461553a6f8eb3ba1b9809e19f6444c
Charles <charles.delafontaine@gmail.com>
2022-03-25 22:19:17 -0400

    VarieteContenuDansUnRang int

---
+++ common/tables/Variete.ts
@@ -0,0 +1,11 @@
+import { Coordonnnes_t } from "../c_types/Coordonnees_t";
+import { DescriptionVariete_t } from '../c_types/DescriptionVariete_t';
+
+export interface VarieteContenuDansUnRang {
+    nom: string;
+    anneeMiseEnMarche: Date;
+    description: DescriptionVariete_t;
+    periodeMiseEnPlace: string;
+    periodeRecolte: string;
+    commentaireGeneral: string;
+}
